edfb0d525c66f4eebfd74768a6933ad6a863b28d
Slivka <max04032007@gmail.com>
2026-02-11 12:52:13 +0200

    Update NoteController.java

--- src/main/java/ua/opnu/todo_list/conroller/NoteController.java
+++ src/main/java/ua/opnu/todo_list/conroller/NoteController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import ua.opnu.todo_list.service.NoteService;
+import ua.opnu.todo_list.entity.Note;
 
 @Controller
 @RequestMapping("/note")
@@ -19,5 +20,24 @@ public class NoteController {
         ModelAndView result = new ModelAndView("note-list");
         result.addObject("notes", noteService.listAll());
         return result;
+    }
+
+    @PostMapping("/delete")
+    public String deleteNote(@RequestParam long id) {
+        noteService.deleteById(id);
+        return "redirect:/note/list";
+    }
+
+    @GetMapping("/edit")
+    public ModelAndView editPage(@RequestParam long id) {
+        ModelAndView result = new ModelAndView("note-edit");
+        result.addObject("note", noteService.getById(id));
+        return result;
+    }
+
+    @PostMapping("/edit")
+    public String saveNote(@ModelAttribute Note note) {
+        noteService.update(note);
+        return "redirect:/note/list";
     }
 }
